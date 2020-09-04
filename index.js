4831eab80c5de0cc589e7498549de47925a6edc8
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-04 18:17:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add function sayHello(name="") {
 function mapToNegativize(src) { 
   r = []
-  for (let i = 0; if i < src.length; i++) { 
+  for (let i = 0; i < src.length; i++) { 
     r.push(src[1] * -1)
   }
   return r 
